37fe5e60b57b05b537b331065d0ed6794d47887b
wtbno <bruno_links@hotmail.com>
2022-12-29 20:29:04 -0300

    cont. aula

--- Aulas Condições/Cond-aninhadas.py
+++ Aulas Condições/Cond-aninhadas.py
@@ -1,3 +1,9 @@
 # Sempre vai ter que ter um IF
 # Pode se usar ELIF quantas vezes for necessário
 # ELSE só se utiliza uma vez.
+
+nome = str(input('Insira seu nome: '))
+if nome == 'Bruno':
+    print('Que bonito nome, {}' .format(nome))
+else:
+    print('Olá {}' .format(nome))
