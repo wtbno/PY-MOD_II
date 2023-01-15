fe9586b5f5ad1450f4e0ff5942dc582e4c1484bd
wtbno <bruno_links@hotmail.com>
2023-01-15 16:40:16 -0300

    Feat.: Missile attack!

--- Aulas repetições (For)/E46/Contagem.py
+++ Aulas repetições (For)/E46/Contagem.py
@@ -1,7 +1,18 @@
+import pygame
 from time import sleep
+import emoji
 # Sleep para ficar com efeito de loading
+
+
 for contagem in range(10, 0, - 1):
-    print('Contagem regressiva...')
+
+    print(emoji.emojize('WARNING, MISSILE ATTACK CONFIRM! :fire:'))
     print(contagem)
-    sleep(3)
-print('Fim da contagem')
+    sleep(1)
+
+print('ROCKET!!!')
+pygame.init()
+pygame.mixer.music.load('nuclear.mp3')
+pygame.mixer.music.play(loops=0, start=0.0)
+input()
+pygame.event.wait()
