b6a9ea3ae8b0cb10c897d9283bc4f6f1abf3b1e4
wtbno <bruno_links@hotmail.com>
2023-05-16 20:44:46 -0300

    cont

---
+++ Aulas Condições (if..elif)/Exercícios/E51/Cont.py
@@ -0,0 +1,6 @@
+primeiro = int(input('Insira um valor: '))
+razao = int(input('Razão: '))
+decimo = primeiro + (10 - 1) * razao
+for c in range(primeiro, decimo + razao, razao):
+    print('{} '.format(c), end=' → ')
+print('Fim')
