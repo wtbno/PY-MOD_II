5d10f806508f0449b4a89e19467e840e8fa04bbb
wtbno <bruno_links@hotmail.com>
2023-06-17 16:20:37 -0300

    E1

---
+++ PythonAcademy/Aula6/Exercício/E1.py
@@ -0,0 +1,11 @@
+nome = 'Bruno'
+sobrenome = 'Alves'
+idade = 32
+ano_Nascimento = 2023 - idade
+maior_Idade = idade >= 18
+altura = 1.75
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Nascido em: ', ano_Nascimento)
+print('É maior de idade? ', maior_Idade)
+print('Altura: ', altura)
---
+++ PythonAcademy/Aula6/Variaveis.py
@@ -0,0 +1,24 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+# nome_completo = 'Bruno de Brito ALves'
+# soma_dois_mais_dois = 2 + 2
+# int_um = bool('1')
+# print(int_um, type(int_um))
+# print(nome_completo, soma_dois_mais_dois)
+
+
+# É uma boa prática que a variável reflita em seu valor de acordo com seu nome
+# int_um = bool('1') => errado
+# int_um = int(1)
+
+
+nome = 'Bruno'
+idade = 17
+maior_de_idade = idade >= 18
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?', maior_de_idade)
