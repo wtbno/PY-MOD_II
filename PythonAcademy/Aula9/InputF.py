f2117c6ff296f7b9385a499cd9a17f649167273e
wtbno <bruno_links@hotmail.com>
2023-06-23 20:57:37 -0300

    feat input

---
+++ PythonAcademy/Aula9/InputF.py
@@ -0,0 +1,14 @@
+# nome = input('Qual o seu nome? ')
+# print(f'O seu nome é {nome }')
+
+
+num1 = int(input('Primeiro número'))
+
+num2 = int(input('Segundo número'))
+
+# Nesse caso, é uma forma de conversão que pode gerar erros se
+# o usuário digitar uma str ao invés do número
+
+# Para checagem
+int_num1 = int(int_num1)
+int_num2 = int(int_num2)
