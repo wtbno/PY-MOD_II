b7261e0805fd549310328fe0b3cefdcd215ef90c
wtbno <bruno_links@hotmail.com>
2023-07-01 16:06:42 -0300

    exercicio maior

---
+++ PythonAcademy/Exercícios/Maior/maior.py
@@ -0,0 +1,8 @@
+primeiro = input("Primeiro valor: ")
+segundo = input("Segundo valor: ")
+
+
+if primeiro > segundo:
+    print('O primeiro valor {} é maior que o segundo {}' .format(primeiro, segundo))
+else:
+    print('O segundo valor {} é maior que o primeiro {}' .format(segundo, primeiro))
